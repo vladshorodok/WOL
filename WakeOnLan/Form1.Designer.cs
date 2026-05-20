df89e3c40af1bd746c7fae00df56b6797f1130b2
vladshorodok <vlad.shorodoc@gmail.com>
2026-05-20 14:29:46 +0300

    Update design

--- WakeOnLan/Form1.Designer.cs
+++ WakeOnLan/Form1.Designer.cs
@@ -29,9 +29,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblStatusDot = new System.Windows.Forms.Label();
             this.btnRefreshPorts = new System.Windows.Forms.Button();
             this.cmbPort = new System.Windows.Forms.ComboBox();
             this.lblPort = new System.Windows.Forms.Label();
@@ -39,24 +42,25 @@
             this.btnStart = new System.Windows.Forms.Button();
             this.lblStatus = new System.Windows.Forms.Label();
             this.dgvUsers = new System.Windows.Forms.DataGridView();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
-            this.btnSave = new System.Windows.Forms.Button();
-            this.grpLog = new System.Windows.Forms.GroupBox();
-            this.lstLogs = new System.Windows.Forms.ListBox();
-            this.btnClearLog = new System.Windows.Forms.Button();
-            this.lblStatusDot = new System.Windows.Forms.Label();
             this.ColName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.PhoneNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.MacAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClearLog = new System.Windows.Forms.Button();
+            this.lstLogs = new System.Windows.Forms.ListBox();
+            this.lblLogs = new System.Windows.Forms.Label();
+            this.btnSaveLogs = new System.Windows.Forms.Button();
+            this.lblCopyRight = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
-            this.grpLog.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(50)))));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(57)))), ((int)(((byte)(71)))));
+            this.panel1.Controls.Add(this.lblCopyRight);
             this.panel1.Controls.Add(this.lblStatusDot);
             this.panel1.Controls.Add(this.btnRefreshPorts);
             this.panel1.Controls.Add(this.cmbPort);
@@ -69,6 +73,17 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1181, 60);
             this.panel1.TabIndex = 0;
+            // 
+            // lblStatusDot
+            // 
+            this.lblStatusDot.AutoSize = true;
+            this.lblStatusDot.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatusDot.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lblStatusDot.Location = new System.Drawing.Point(12, 15);
+            this.lblStatusDot.Name = "lblStatusDot";
+            this.lblStatusDot.Size = new System.Drawing.Size(26, 30);
+            this.lblStatusDot.TabIndex = 6;
+            this.lblStatusDot.Text = "●";
             // 
             // btnRefreshPorts
             // 
@@ -150,31 +165,70 @@
             this.dgvUsers.AllowUserToResizeRows = false;
             this.dgvUsers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
             this.dgvUsers.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.dgvUsers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ColName,
             this.PhoneNumber,
             this.MacAddress});
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(170)))), ((int)(((byte)(212)))));
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvUsers.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(170)))), ((int)(((byte)(187)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvUsers.DefaultCellStyle = dataGridViewCellStyle6;
             this.dgvUsers.GridColor = System.Drawing.SystemColors.Control;
             this.dgvUsers.Location = new System.Drawing.Point(699, 150);
             this.dgvUsers.MultiSelect = false;
             this.dgvUsers.Name = "dgvUsers";
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUsers.RowHeadersDefaultCellStyle = dataGridViewCellStyle7;
             this.dgvUsers.RowHeadersVisible = false;
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dgvUsers.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle8.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(170)))), ((int)(((byte)(206)))));
+            this.dgvUsers.RowsDefaultCellStyle = dataGridViewCellStyle8;
             this.dgvUsers.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.dgvUsers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvUsers.Size = new System.Drawing.Size(345, 150);
             this.dgvUsers.TabIndex = 2;
+            // 
+            // ColName
+            // 
+            this.ColName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ColName.FillWeight = 150F;
+            this.ColName.HeaderText = "Name";
+            this.ColName.Name = "ColName";
+            // 
+            // PhoneNumber
+            // 
+            this.PhoneNumber.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.PhoneNumber.FillWeight = 150F;
+            this.PhoneNumber.HeaderText = "Numar Telefon";
+            this.PhoneNumber.Name = "PhoneNumber";
+            this.PhoneNumber.Width = 102;
+            // 
+            // MacAddress
+            // 
+            this.MacAddress.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.MacAddress.FillWeight = 150F;
+            this.MacAddress.HeaderText = "Adresa MAC";
+            this.MacAddress.Name = "MacAddress";
             // 
             // btnAdd
             // 
@@ -217,35 +271,13 @@
             this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
-            // grpLog
-            // 
-            this.grpLog.Controls.Add(this.lstLogs);
-            this.grpLog.Location = new System.Drawing.Point(37, 131);
-            this.grpLog.Name = "grpLog";
-            this.grpLog.Size = new System.Drawing.Size(589, 276);
-            this.grpLog.TabIndex = 6;
-            this.grpLog.TabStop = false;
-            this.grpLog.Text = "Logs";
-            // 
-            // lstLogs
-            // 
-            this.lstLogs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(40)))));
-            this.lstLogs.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lstLogs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(255)))), ((int)(((byte)(180)))));
-            this.lstLogs.FormattingEnabled = true;
-            this.lstLogs.ItemHeight = 14;
-            this.lstLogs.Location = new System.Drawing.Point(0, 19);
-            this.lstLogs.Name = "lstLogs";
-            this.lstLogs.Size = new System.Drawing.Size(589, 242);
-            this.lstLogs.TabIndex = 0;
-            // 
             // btnClearLog
             // 
             this.btnClearLog.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
             this.btnClearLog.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnClearLog.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClearLog.ForeColor = System.Drawing.Color.White;
-            this.btnClearLog.Location = new System.Drawing.Point(37, 413);
+            this.btnClearLog.Location = new System.Drawing.Point(37, 388);
             this.btnClearLog.Name = "btnClearLog";
             this.btnClearLog.Size = new System.Drawing.Size(75, 23);
             this.btnClearLog.TabIndex = 7;
@@ -253,46 +285,62 @@
             this.btnClearLog.UseVisualStyleBackColor = false;
             this.btnClearLog.Click += new System.EventHandler(this.btnClearLog_Click_1);
             // 
-            // lblStatusDot
+            // lstLogs
             // 
-            this.lblStatusDot.AutoSize = true;
-            this.lblStatusDot.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStatusDot.ForeColor = System.Drawing.Color.OrangeRed;
-            this.lblStatusDot.Location = new System.Drawing.Point(12, 15);
-            this.lblStatusDot.Name = "lblStatusDot";
-            this.lblStatusDot.Size = new System.Drawing.Size(26, 30);
-            this.lblStatusDot.TabIndex = 6;
-            this.lblStatusDot.Text = "●";
+            this.lstLogs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(88)))), ((int)(((byte)(114)))));
+            this.lstLogs.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstLogs.ForeColor = System.Drawing.Color.White;
+            this.lstLogs.FormattingEnabled = true;
+            this.lstLogs.ItemHeight = 14;
+            this.lstLogs.Location = new System.Drawing.Point(37, 140);
+            this.lstLogs.Name = "lstLogs";
+            this.lstLogs.Size = new System.Drawing.Size(589, 242);
+            this.lstLogs.TabIndex = 0;
             // 
-            // ColName
+            // lblLogs
             // 
-            this.ColName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.ColName.FillWeight = 150F;
-            this.ColName.HeaderText = "Name";
-            this.ColName.Name = "ColName";
+            this.lblLogs.AutoSize = true;
+            this.lblLogs.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogs.Location = new System.Drawing.Point(40, 122);
+            this.lblLogs.Name = "lblLogs";
+            this.lblLogs.Size = new System.Drawing.Size(32, 15);
+            this.lblLogs.TabIndex = 8;
+            this.lblLogs.Text = "Logs";
             // 
-            // PhoneNumber
+            // btnSaveLogs
             // 
-            this.PhoneNumber.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
-            this.PhoneNumber.FillWeight = 150F;
-            this.PhoneNumber.HeaderText = "Numar Telefon";
-            this.PhoneNumber.Name = "PhoneNumber";
-            this.PhoneNumber.Width = 102;
+            this.btnSaveLogs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.btnSaveLogs.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSaveLogs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveLogs.ForeColor = System.Drawing.Color.White;
+            this.btnSaveLogs.Location = new System.Drawing.Point(130, 388);
+            this.btnSaveLogs.Name = "btnSaveLogs";
+            this.btnSaveLogs.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveLogs.TabIndex = 9;
+            this.btnSaveLogs.Text = "Save Logs";
+            this.btnSaveLogs.UseVisualStyleBackColor = false;
             // 
-            // MacAddress
+            // lblCopyRight
             // 
-            this.MacAddress.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.MacAddress.FillWeight = 150F;
-            this.MacAddress.HeaderText = "Adresa MAC";
-            this.MacAddress.Name = "MacAddress";
+            this.lblCopyRight.AutoSize = true;
+            this.lblCopyRight.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyRight.ForeColor = System.Drawing.Color.White;
+            this.lblCopyRight.Location = new System.Drawing.Point(962, 13);
+            this.lblCopyRight.Name = "lblCopyRight";
+            this.lblCopyRight.Size = new System.Drawing.Size(207, 34);
+            this.lblCopyRight.TabIndex = 7;
+            this.lblCopyRight.Text = "POWERTRACE SRL Coypright 2026\r\nAutor: Vladislav Shorodok\r\n";
+            this.lblCopyRight.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1181, 579);
+            this.Controls.Add(this.btnSaveLogs);
+            this.Controls.Add(this.lblLogs);
+            this.Controls.Add(this.lstLogs);
             this.Controls.Add(this.btnClearLog);
-            this.Controls.Add(this.grpLog);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAdd);
@@ -303,8 +351,8 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
-            this.grpLog.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -318,9 +366,7 @@
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.GroupBox grpLog;
         private System.Windows.Forms.Button btnClearLog;
-        private System.Windows.Forms.ListBox lstLogs;
         private System.Windows.Forms.Button btnRefreshPorts;
         private System.Windows.Forms.ComboBox cmbPort;
         private System.Windows.Forms.Label lblPort;
@@ -328,6 +374,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn ColName;
         private System.Windows.Forms.DataGridViewTextBoxColumn PhoneNumber;
         private System.Windows.Forms.DataGridViewTextBoxColumn MacAddress;
+        private System.Windows.Forms.Label lblCopyRight;
+        private System.Windows.Forms.ListBox lstLogs;
+        private System.Windows.Forms.Label lblLogs;
+        private System.Windows.Forms.Button btnSaveLogs;
     }
 }
 
